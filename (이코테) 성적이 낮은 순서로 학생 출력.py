0a432662f1efcca75ad43950bd2f3c93bcf3cfb7
Ryu$moothie <68692949+SoominRyu@users.noreply.github.com>
2021-09-21 19:52:50 +0900

    Create (이코테) 성적이 낮은 순서로 학생 출력.py

---
+++ (이코테) 성적이 낮은 순서로 학생 출력.py
@@ -0,0 +1,9 @@
+n = int(input())
+a = []
+for i in range(n):
+  student = input().split()
+  a.append((student[0], int(student[1])))
+print(a[1])
+a = sorted(a, key=lambda grade:grade[1])
+for grade in a:
+  print(grade[0],end=' ')
